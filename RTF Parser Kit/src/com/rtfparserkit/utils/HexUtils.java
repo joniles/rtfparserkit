bf1a7906c81ecabdff25e3743d5aadc4fbceaa8a
Jon Iles <joniles@users.sourceforge.net>
2013-12-15 21:30:31 +0000

    Updated to make hex digit conversion more efficient.

--- RTF Parser Kit/src/com/rtfparserkit/utils/HexUtils.java
+++ RTF Parser Kit/src/com/rtfparserkit/utils/HexUtils.java
@@ -16,6 +16,8 @@
 
 package com.rtfparserkit.utils;
 
+import java.util.Arrays;
+
 /**
  * Utilities for working with hex numbers.
  */
@@ -27,29 +29,22 @@ public class HexUtils
    public static int parseHexDigit(int ch)
    {
       int b;
-      if (Character.isDigit(ch))
+
+      try
       {
-         b = ch - '0';
+         b = HEX_MAP[ch];
       }
-      else
+
+      catch (IndexOutOfBoundsException ex)
       {
-         if (Character.isLowerCase(ch))
-         {
-            if (ch < 'a' || ch > 'f')
-            {
-               throw new IllegalArgumentException("Invalid hex digit " + ch);
-            }
-            b = (char) ch - 'a' + 10;
-         }
-         else
-         {
-            if (ch < 'A' || ch > 'F')
-            {
-               throw new IllegalArgumentException("Invalid hex digit " + ch);
-            }
-            b = (char) ch - 'A' + 10;
-         }
+         b = -1;
       }
+
+      if (b == -1)
+      {
+         throw new IllegalArgumentException("Invalid hex digit " + ch);
+      }
+
       return b;
    }
 
@@ -73,5 +68,36 @@ public class HexUtils
       }
 
       return bytes;
+   }
+
+   private static final int[] HEX_MAP = new int['g'];
+   static
+   {
+      Arrays.fill(HEX_MAP, -1);
+
+      HEX_MAP['0'] = 0;
+      HEX_MAP['1'] = 1;
+      HEX_MAP['2'] = 2;
+      HEX_MAP['3'] = 3;
+      HEX_MAP['4'] = 4;
+      HEX_MAP['5'] = 5;
+      HEX_MAP['6'] = 6;
+      HEX_MAP['7'] = 7;
+      HEX_MAP['8'] = 8;
+      HEX_MAP['9'] = 9;
+
+      HEX_MAP['A'] = 10;
+      HEX_MAP['B'] = 11;
+      HEX_MAP['C'] = 12;
+      HEX_MAP['D'] = 13;
+      HEX_MAP['E'] = 14;
+      HEX_MAP['F'] = 15;
+
+      HEX_MAP['a'] = 10;
+      HEX_MAP['b'] = 11;
+      HEX_MAP['c'] = 12;
+      HEX_MAP['d'] = 13;
+      HEX_MAP['e'] = 14;
+      HEX_MAP['f'] = 15;
    }
 }
