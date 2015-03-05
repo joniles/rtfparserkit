2bf42c3a786a37e336ae56f9c0d1775b81ec06e3
Jon Iles <jon.iles@bcs.org.uk>
2015-03-05 11:54:58 +0000

    Updated comments.

--- RTF Parser Kit/src/com/rtfparserkit/rtf/CommandType.java
+++ RTF Parser Kit/src/com/rtfparserkit/rtf/CommandType.java
@@ -17,9 +17,14 @@
 package com.rtfparserkit.rtf;
 
 /**
- * Enumeration of command types.
+ * Enumeration of command types. See Appendix B of the RTF specification.
  */
 public enum CommandType
 {
-   Symbol, Flag, Toggle, Value, Destination, Encoding;
+   Symbol, // This control word represents a special character 
+   Flag, // This control word ignores any parameter.
+   Toggle, // This control word distinguishes between the ON and OFF states for the given property
+   Value, // This control word requires a parameter.
+   Destination, // This control word starts a group or destination. It ignores any parameter. 
+   Encoding; // Switch the character encoding used from this point in the document
 }
